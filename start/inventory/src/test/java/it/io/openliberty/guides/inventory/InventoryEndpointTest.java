ff2c139ecdf61a6e6dd3da80baadd6546c8c161a
Mai Hameed <maihameed@hotmail.com>
2019-12-18 17:40:53 -0500

    Updated to JUnit5 and verified ITs

--- start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointTest.java
+++ start/inventory/src/test/java/it/io/openliberty/guides/inventory/InventoryEndpointTest.java
@@ -10,45 +10,43 @@
  *******************************************************************************/
 package it.io.openliberty.guides.inventory;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.TestMethodOrder;
+
+import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.SSLSession;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
-import javax.json.JsonObject;
 import javax.ws.rs.core.MediaType;
+import javax.json.JsonObject;
 
-import org.apache.cxf.jaxrs.provider.jsrjsonp.JsrJsonpProvider;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-public class InventoryEndpointTest {
+@TestMethodOrder(OrderAnnotation.class)
+public class InventoryEndpointIT {
 
     private static String invUrl;
     private static String sysUrl;
     private static String sysKubeService;
 
-    private Client client;
-    private Response response;
-
-    @BeforeClass
-    public static void oneTimeSetup() {
+    private static Client client;
+ 
+    @BeforeAll
+    public static void setup() {
         String invIP = System.getProperty("inventory.ip");
         String sysIP = System.getProperty("system.ip");
         
         sysKubeService = System.getProperty("system.kube.service");
         invUrl = "http://" + invIP + "/inventory/systems/";
         sysUrl = "http://" + sysIP + "/system/properties/";
-    }
-
-    @Before
-    public void setup() {
-        response = null;
+  
         client = ClientBuilder.newBuilder()
                     .hostnameVerifier(new HostnameVerifier() {
                         public boolean verify(String hostname, SSLSession session) {
@@ -61,19 +59,13 @@ public class InventoryEndpointTest {
         client.target(invUrl + "reset").request().post(null);
     }
 
-    @After
-    public void teardown() {
+    @AfterAll
+    public static void teardown() {
         client.close();
     }
 
     @Test
-    public void testSuite() {
-        this.testEmptyInventory();
-        this.testHostRegistration();
-        this.testSystemPropertiesMatch();
-        this.testUnknownHost();
-    }
-
+    @Order(1)
     public void testEmptyInventory() {
         Response response = this.getResponse(invUrl);
         this.assertResponse(invUrl, response);
@@ -82,12 +74,14 @@ public class InventoryEndpointTest {
 
         int expected = 0;
         int actual = obj.getInt("total");
-        assertEquals("The inventory should be empty on application start but it wasn't",
-                    expected, actual);
+        assertEquals(expected, actual,
+            "The inventory should be empty on application start but it wasn't");
 
         response.close();
     }
 
+    @Test
+    @Order(2)
     public void testHostRegistration() {
         this.visitSystemService();
 
@@ -98,18 +92,20 @@ public class InventoryEndpointTest {
 
         int expected = 1;
         int actual = obj.getInt("total");
-        assertEquals("The inventory should have one entry for " + sysKubeService, expected,
-                    actual);
+        assertEquals(expected, actual, 
+            "The inventory should have one entry for " + sysKubeService);
 
         boolean serviceExists = obj.getJsonArray("systems").getJsonObject(0)
                                     .get("hostname").toString()
                                     .contains(sysKubeService);
-        assertTrue("A host was registered, but it was not " + sysKubeService,
-                serviceExists);
+        assertTrue(serviceExists,
+            "A host was registered, but it was not " + sysKubeService);
 
         response.close();
     }
 
+    @Test
+    @Order(3)
     public void testSystemPropertiesMatch() {
         Response invResponse = this.getResponse(invUrl);
         Response sysResponse = this.getResponse(sysUrl);
@@ -138,6 +134,8 @@ public class InventoryEndpointTest {
         sysResponse.close();
     }
 
+    @Test
+    @Order(4)
     public void testUnknownHost() {
         Response response = this.getResponse(invUrl);
         this.assertResponse(invUrl, response);
@@ -149,8 +147,8 @@ public class InventoryEndpointTest {
         String obj = badResponse.readEntity(String.class);
 
         boolean isError = obj.contains("ERROR");
-        assertTrue("badhostname is not a valid host but it didn't raise an error",
-                isError);
+        assertTrue(isError,
+            "badhostname is not a valid host but it didn't raise an error");
 
         response.close();
         badResponse.close();
@@ -180,8 +178,8 @@ public class InventoryEndpointTest {
      *          - response received from the target URL.
      */
     private void assertResponse(String url, Response response) {
-        assertEquals("Incorrect response code from " + url, 200,
-                    response.getStatus());
+        assertEquals(200, response.getStatus(), 
+            "Incorrect response code from " + url);
     }
 
     /**
@@ -199,9 +197,10 @@ public class InventoryEndpointTest {
      */
     private void assertProperty(String propertyName, String hostname,
         String expected, String actual) {
-        assertEquals("JVM system property [" + propertyName + "] "
+        assertEquals(expected, actual,
+            "JVM system property [" + propertyName + "] "
             + "in the system service does not match the one stored in "
-            + "the inventory service for " + hostname, expected, actual);
+            + "the inventory service for " + hostname);
     }
 
     /**
