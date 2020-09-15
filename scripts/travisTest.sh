931aea9b696acd3c2b60106af814b5548c4dab68
Gilbert Kwan <gkwan@ca.ibm.com>
2020-09-15 18:39:27 -0400

    Update travisTest.sh

--- scripts/travisTest.sh
+++ scripts/travisTest.sh
@@ -18,7 +18,7 @@ docker build -t `oc registry info`/`oc project -q`/inventory:test inventory/.
 
 oc apply -f ../scripts/test.yaml
 
-sleep 120
+sleep 60
 
 oc get pods
 
@@ -29,11 +29,11 @@ oc get routes
 SYSTEM_IP=`oc get route system-route -o=jsonpath='{.spec.host}'`
 INVENTORY_IP=`oc get route inventory-route -o=jsonpath='{.spec.host}'`
 
-timeout=24
+timeout=20
 curl http://$SYSTEM_IP/system/properties
 count=`curl http://$INVENTORY_IP/inventory/systems/system-service | grep -c Time-out` || true
 while (( $count > 0 && $timeout != 0 )); do
-    echo wait for a while...$timeout; sleep 5; 
+    echo wait for a while...$timeout; sleep 15; 
     timeout=$(($timeout-1));
     count=`curl http://$INVENTORY_IP/inventory/systems/system-service | grep -c Time-out` || true; 
 done
