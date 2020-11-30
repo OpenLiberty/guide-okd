e4ec1f65a312ac5acac66c17218983829152e826
Austin Seto <austin.seto@ibm.com>
2020-11-30 15:19:37 -0500

    update tests

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -14,18 +14,6 @@ sudo apt-get install -y conntrack
 
 sudo minikube start --vm-driver=none --bootstrapper=kubeadm
 
-# Set up OC
-
-sudo mount --make-rshared /
-wget -nv https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
-cd openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
-export PATH=`pwd`:$PATH
-cd ..
-sudo mv ../scripts/daemon.json /etc/docker/
-sudo systemctl restart docker
-oc cluster up
-
 # Test app
 
 mvn -q package
