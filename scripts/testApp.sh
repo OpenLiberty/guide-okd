e252a16dcd9d52a2df94d3b9016203198692607f
Austin Seto <austin.seto@ibm.com>
2020-11-30 15:38:26 -0500

    Change setup

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 set -euxo pipefail
 
-# Set up Minikube
+# Set up Openshift
 
-curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-chmod +x kubectl
-sudo ln -s $(pwd)/kubectl /usr/local/bin/kubectl
-wget https://github.com/kubernetes/minikube/releases/download/v0.28.2/minikube-linux-amd64 -q -O minikube
-chmod +x minikube
-
-sudo apt-get update -y
-sudo apt-get install -y conntrack
-
-sudo minikube start --vm-driver=none --bootstrapper=kubeadm
+sudo mount --make-rshared /
+wget -nv https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+tar -xvf openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz
+cd openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit
+export PATH=`pwd`:$PATH
+cd ..
+sudo mv ../scripts/daemon.json /etc/docker/
+sudo systemctl restart docker
+oc cluster up
 
 # Test app
 
