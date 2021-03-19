43867c8f9ab2d577c618ea4305a4f5c641ac94ad
Tien-Thanh Le <tienthanh.le@mail.utoronto.ca>
2021-03-19 13:26:44 -0400

    get pods

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -35,7 +35,7 @@ while ((count > 0 && timeout != 0)); do
     count="$(curl http://"$INVENTORY_IP"/inventory/systems/system-service | grep -c Time-out)" || true
 done
 
-docker ps
+oc get pods
 
 mvn verify -Ddockerfile.skip=true -Dsystem.ip="$SYSTEM_IP" -Dinventory.ip="$INVENTORY_IP"
 
