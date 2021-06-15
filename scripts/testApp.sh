9b03ac60be851daee0a3d6364b5da2878a15427d
Tien Thanh Le <44335153+tt-le@users.noreply.github.com>
2021-06-15 11:44:16 -0400

    Update testApp.sh

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -10,14 +10,14 @@ mvn -Dhttp.keepAlive=false \
 
 oc registry login --skip-check=true
 
-docker pull openliberty/open-liberty:kernel-java8-openj9-ubi
+docker pull openliberty/open-liberty:full-java11-openj9-ubi
 
-docker build -t `oc registry info`/`oc project -q`/system:test system/.
-docker build -t `oc registry info`/`oc project -q`/inventory:test inventory/.
+docker build -t "$(oc registry info)/$(oc project -q)/system:test" system/.
+docker build -t "$(oc registry info)/$(oc project -q)/inventory:test" inventory/.
 
 oc apply -f ../scripts/test.yaml
 
-sleep 120
+sleep 60
 
 oc get pods
 
@@ -25,21 +25,21 @@ oc describe pods
 
 oc get routes
 
-SYSTEM_IP=`oc get route system-route -o=jsonpath='{.spec.host}'`
-INVENTORY_IP=`oc get route inventory-route -o=jsonpath='{.spec.host}'`
+SYSTEM_IP=$(oc get route system-route -o=jsonpath='{.spec.host}')
+INVENTORY_IP=$(oc get route inventory-route -o=jsonpath='{.spec.host}')
 
 timeout=20
-curl http://$SYSTEM_IP/system/properties
-count=`curl http://$INVENTORY_IP/inventory/systems/system-service | grep -c Time-out` || true
-while (( $count > 0 && $timeout != 0 )); do
+curl http://"$SYSTEM_IP"/system/properties
+count=$(curl http://"$INVENTORY_IP"/inventory/systems/system-service | grep -c Time-out) || true
+while (( count > 0 && timeout != 0 )); do
     echo wait for a while...$timeout; sleep 15; 
-    timeout=$(($timeout-1));
-    count=`curl http://$INVENTORY_IP/inventory/systems/system-service | grep -c Time-out` || true; 
+    timeout=$((timeout-1));
+    count=$(curl http://"$INVENTORY_IP"/inventory/systems/system-service | grep -c Time-out) || true; 
 done
 
-mvn verify -Ddockerfile.skip=true -Dsystem.ip=$SYSTEM_IP -Dinventory.ip=$INVENTORY_IP
+mvn verify -Ddockerfile.skip=true -Dsystem.ip="$SYSTEM_IP" -Dinventory.ip="$INVENTORY_IP"
 
-oc logs $(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)
-oc logs $(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)
+oc logs "$(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep system)"
+oc logs "$(oc get pods -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}' | grep inventory)"
 
 oc delete -f ../scripts/test.yaml
