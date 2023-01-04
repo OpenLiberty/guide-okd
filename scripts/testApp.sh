254d3a7ba3e454431e125d8a80106f7fc9b3a16a
Jakub Pomykala <jakub.pomykala@hotmail.co.uk>
2023-01-04 13:10:34 +0000

    Adding missing -ntp

--- scripts/testApp.sh
+++ scripts/testApp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euxo pipefail
 
-mvn -Dhttp.keepAlive=false \
+mvn -ntp -Dhttp.keepAlive=false \
     -Dmaven.wagon.http.pool=false \
     -Dmaven.wagon.httpconnectionManager.ttlSeconds=120 \
     -q clean package
